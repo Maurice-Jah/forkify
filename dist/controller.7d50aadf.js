1ade1069b71288696b9ee2955571178d79a1b86c
Maurice-Jah <mauricejahwizwid@gmail.com>
2023-09-25 04:27:43 +0100

    Add documentation

--- dist/controller.7d50aadf.js
+++ dist/controller.7d50aadf.js
@@ -2876,6 +2876,16 @@ var _icons = _interopRequireDefault(require("url:../../img/icons.svg"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 class View {
   _data;
+
+  /**
+   * Render the received object to the DOM
+   * @param  {Object | Object[]}  data The data to be rendered (e.g recipe)
+   * @param {boolean} [render = true] if false, create markup string instead of rendering to the DOM
+   * @returns {undefined | string} A markup string is returned if render is false
+   * @this {Object} view instance
+   * @author Maurice
+   */
+
   render(data) {
     let render = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : true;
     if (!data || Array.isArray(data) && data.length === 0) return this.renderError();
